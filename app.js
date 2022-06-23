144a29389fec3e21c148d2d71ae3ead7b9fda1b4
geRIvera <griveram@summa-sci.com>
2022-06-23 06:51:58 -0500

    modificado app.js y views/home.html

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express'); //invoca el metodo de express en node modules
+const app = express();
+
 const path = require('path'); //unifica las rutas entre los sistemas operativos
-const app = express(); 
 const publicPath = path.resolve(__dirname,'./public'); // resolve resuelve la ruta indicada
 
 app.use(express.static(publicPath)); //permite el uso de archivos de una manera estática, le indica a express que es un recurso
@@ -11,9 +12,4 @@ app.listen(3000,()=> {
 
 app.get('/',(request,response)=>{
     response.sendFile(path.resolve(__dirname,'./views/home.html'));//permite enviar archivo al navegador
-});
-
-
-app.get('/',(request,response)=>{
-    response.sendFile(path.resolve(__dirname,'./views/ofertas.html'));//permite enviar archivo al navegador
 });
