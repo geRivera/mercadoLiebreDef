6901b0fb1ebeb0741dfc68232b8ee1474fcd2e78
geRIvera <griveram@summa-sci.com>
2022-07-04 18:42:13 -0500

    cambio register

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const publicPath = path.resolve(__dirname,'./public'); // resolve resuelve la ru
 
 app.use(express.static(publicPath)); //permite el uso de archivos de una manera estática, le indica a express que es un recurso
 
-app.listen(3000,()=> {
+//app.listen(3000,()=> {
+    //console.log('Servidor vivo en puerto 3000'); //para levantar el servidor
+//});
+
+app.listen(process.env.PORT || 3000 function() {
     console.log('Servidor vivo en puerto 3000'); //para levantar el servidor
-});
+})
 
 app.get('/',(request,response)=>{
     response.sendFile(path.resolve(__dirname,'./views/home.html'));//permite enviar archivo al navegador
