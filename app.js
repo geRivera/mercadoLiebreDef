ad4bf65a4a469beb9a85d9e362ed44a6216c7ab9
geRIvera <griveram@summa-sci.com>
2022-07-04 19:11:39 -0500

    puerto modificado

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ app.use(express.static(publicPath)); //permite el uso de archivos de una manera 
     //console.log('Servidor vivo en puerto 3000'); //para levantar el servidor
 //});
 
-app.listen(process.env.PORT || 3000 function() {
+app.listen(process.env.PORT || 3000, ()=> {
     console.log('Servidor vivo en puerto 3000'); //para levantar el servidor
-})
+});
 
 app.get('/',(request,response)=>{
     response.sendFile(path.resolve(__dirname,'./views/home.html'));//permite enviar archivo al navegador
